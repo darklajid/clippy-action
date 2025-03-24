675fad9d305017a0f61f20a19f15127f01c61fb7
Benjamin Podszun <ben.podszun@cyberowl.io>
2025-03-24 19:00:47 +0100

    Move from fail-on-error to fail-level

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function run(): Promise<void> {
     const level = core.getInput('level') || 'error'
     const reporter = core.getInput('reporter') || 'github-pr-check'
     const filterMode = core.getInput('filter_mode') || 'added'
-    const failOnError = core.getInput('fail_on_error') || 'false'
+    const fail_level = core.getInput('fail_level') || 'none'
     const reviewdogFlags = core.getInput('reviewdog_flags')
     const workdir = core.getInput('workdir') || '.'
     const cwd = path.relative(
@@ -101,7 +101,7 @@ async function run(): Promise<void> {
             `-name=${toolName}`,
             `-reporter=${reporter}`,
             `-filter-mode=${filterMode}`,
-            `-fail-on-error=${failOnError}`,
+            `-fail-level=${fail_level}`,
             `-level=${level}`,
             ...parse(reviewdogFlags)
           ],
